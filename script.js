c6baea3b0c1184b66ace1c492fa8d8eb7ee9eb92
Ahmed <aaa26770123@gmail.com>
2025-02-01 18:33:05 -0700

    Create timer with simple layout

---
+++ script.js
@@ -0,0 +1,21 @@
+const startDateTime = new Date("2024-10-13T09:16:20");
+
+function updateTimer() {
+    const currentDateTime = new Date();
+    const difference = currentDateTime - startDateTime;
+
+    const seconds = Math.floor(difference / 1000) % 60;
+    const minutes = Math.floor(difference / (1000 * 60)) % 60;
+    const hours = Math.floor(difference / (1000 * 60 * 60)) % 24;
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+    document.getElementById("seconds").textContent = seconds;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("days").textContent = days;
+}
+
+const updateInterval_milliseconds = 1000;
+
+setInterval(updateTimer, updateInterval_milliseconds);
+updateTimer();
